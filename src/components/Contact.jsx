c494adc207669bae10de39703d2e27db4a695a08
KipletingEdwin <kipletingedwin4@gmail.com>
2026-09-08 21:26:48 +0100

    add contact part

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 
+import { Mail, MapPin, Phone } from 'lucide-react';
 import React, { useState } from 'react'
 
 const Contact = () => {
@@ -18,6 +19,22 @@ const Contact = () => {
         <div>
           <h2>Contact Us</h2>
           <p> Have a project in mind? Let's connect and discuss how I can help bring your ideas to life. </p>
+        </div>
+        <div>
+          <div>
+            <h3>Get in touch today</h3> 
+            <p> I'm always open to discussing new projects, creative ideas, or opportunities to be part of your vision. </p>
+            <ul>
+              <li><Mail size={18}/>kipletingedwin4@gmail.com</li>
+              <li><Phone size={18}/>+92 3000000</li>
+              <li><MapPin size={18}/>London, UK</li>
+            </ul>
+          </div>
+          <form>
+            <div>
+              
+            </div>
+          </form>
         </div>
       </div>
     </section>
